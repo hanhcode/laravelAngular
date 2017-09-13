c26627d739f90dd613dfb74c0c89994093fd2f85
hanhcode <hanhcode@gmail.com>
2017-09-14 02:20:05 +0700

    Normalize line endings

---
+++ src/app/hanhcom/hanhcom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hanhcom',
+  templateUrl: './hanhcom.component.html',
+  styleUrls: ['./hanhcom.component.css']
+})
+export class HanhcomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
